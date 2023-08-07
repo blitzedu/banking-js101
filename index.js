e533ff1765d57b801c38ee476fb56fc1eacb78af
LandonHarter <landonharter@outlook.com>
2023-08-07 02:45:42 -0400

    challenge code

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
+const password = "1234"
+let verified = false
+
 let money = 0
+let loanedMoney = 0
+
+function verifyPassword() {
+    if (verified) return true;
+
+    console.log("You have not verified your password yet! To verify, type 'verify <password>'")
+    return false;
+}
+
 function processCommand(command) {
     // Process all commands
     if (command === "quit") {
@@ -19,14 +31,48 @@ function processCommand(command) {
     const commandData = cmdSegments[1]
 
     if (commandAction === "deposit") {
+        if (!verifyPassword()) return;
+
         money += parseInt(commandData)
         console.log("Success!")
     } else if (commandAction === "withdraw") {
+        if (!verifyPassword()) return;
+
         if (money < parseInt(commandData)) {
             console.log("Insufficient funds!")
         } else {
             money -= parseInt(commandData)
             console.log("Success!")
+        }
+    } else if (commandAction === "balance") {
+        if (!verifyPassword()) return;
+
+        console.log(`Your balance is ${money}$`)
+        if (loanedMoney > 0) {
+            console.log(`You owe ${loanedMoney}$`)
+        }
+    } else if (commandAction === "loan") {
+        if (!verifyPassword()) return;
+
+        money += parseInt(commandData)
+        loanedMoney += parseInt(commandData)
+        console.log("Success!")
+    } else if (commandAction === "pay") {
+        if (!verifyPassword()) return;
+
+        money -= parseInt(commandData)
+        loanedMoney -= parseInt(commandData)
+        console.log("Success!")
+    } else if (commandAction === "loan-balance") {
+        if (!verifyPassword()) return;
+
+        console.log(`You have loaned ${loanedMoney}`)
+    } else if (commandAction === "verify") {
+        if (commandData === password) {
+            verified = true
+            console.log("Success!")
+        } else {
+            console.log("Incorrect password!")
         }
     }
 }
